955308b377b5ebda9aa2cb5fbc8ffeb781abfa28
et9719 <ella-t@live.co.uk>
2022-05-26 16:39:08 +0000

    Removed secret key and set debug

--- stepldn/settings.py
+++ stepldn/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y#_96_1bne+tm9rmsbx9%&fxk!+o+^g5$kmfsvci6#%6a1x0j&'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

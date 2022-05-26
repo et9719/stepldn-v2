216d0e81bc1162b860f15fda6cdddda3f0dafdee
et9719 <ella-t@live.co.uk>
2022-05-26 16:05:20 +0000

    Allow hosts

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn heroku_cli.wsgi
+web: gunicorn steplondon.wsgi:application
--- stepldn/settings.py
+++ stepldn/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-y#_96_1bne+tm9rmsbx9%&fxk!+o+^g5$kmfsvci6#%6a1x0j&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['steplondon.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'stepldn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation

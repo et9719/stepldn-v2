cc5fd4ce256bd204597bdfe4d1d8a6de353c8516
et9719 <ella-t@live.co.uk>
2022-06-01 18:30:58 +0000

    Add url success

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,12 @@
-''' Imports '''
+''' Imports'''
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
     path('', views.contact_view, name='contact'),
-    path('success', views.success, name='contact/success'),
+    path('success', views.contact_view, name='success'),
+    # path('add/<item_id>/', views.add_to_bag, name='add_to_bag'),
+    # path('adjust/<item_id>/', views.adjust_bag, name='adjust_bag'),
+    # path('remove/<item_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]

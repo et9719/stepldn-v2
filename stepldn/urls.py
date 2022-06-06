142b09091e3d9d53fd694bceb265fba8f54d5556
et9719 <ella-t@live.co.uk>
2022-06-06 12:59:49 +0000

    Edit urls.py- stepldn

--- stepldn/urls.py
+++ stepldn/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    path('', include('pages.urls')),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),

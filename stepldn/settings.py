24bb71853dd7d374a02590a9baa7c320ab279887
et9719 <ella-t@live.co.uk>
2023-03-01 12:34:26 +0000

    Add build.sh file remove procfile and add RENDER_EXTERNAL_HOSTNAME in settings.py

--- stepldn/settings.py
+++ stepldn/settings.py
@@ -22,13 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'f1c9_s_%99i*0s+^%ol^3@#2')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELPOMENT' in os.environ
 
 ALLOWED_HOSTS = ['steplondon.herokuapp.com', 'localhost']
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+  ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
